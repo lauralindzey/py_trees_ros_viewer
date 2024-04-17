a63b9d2bab5a69614cfb85cfdbea18db28452641
Laura Lindzey <lindzey@uw.edu>
2024-04-17 02:14:36 -0700

    update conversions.py to use uuid_msgs

--- py_trees_ros_viewer/conversions.py
+++ py_trees_ros_viewer/conversions.py
@@ -16,15 +16,16 @@ Demo trees to feed to the web app
 
 import uuid
 
+import uuid_msgs.msg
+
 import py_trees_ros_interfaces.msg as py_trees_msgs
-import unique_identifier_msgs.msg as unique_identifier_msgs
 
 ##############################################################################
 # Methods
 ##############################################################################
 
 
-def msg_to_uuid4(msg: unique_identifier_msgs.UUID) -> uuid.UUID:
+def msg_to_uuid4(msg: uuid_msgs.msg.UniqueID) -> uuid.UUID:
     """
     Convert a uuid4 python object to a ros unique identifier, UUID type.
 
@@ -53,17 +54,17 @@ def msg_constant_to_behaviour_str(value: int) -> str:
         TypeError: if the message type is unrecognised
     """
     if value == py_trees_msgs.Behaviour.SEQUENCE:
-        return 'Sequence'
+        return "Sequence"
     elif value == py_trees_msgs.Behaviour.CHOOSER:
-        return 'Chooser'
+        return "Chooser"
     elif value == py_trees_msgs.Behaviour.SELECTOR:
-        return 'Selector'
+        return "Selector"
     elif value == py_trees_msgs.Behaviour.PARALLEL:
-        return 'Parallel'
+        return "Parallel"
     elif value == py_trees_msgs.Behaviour.DECORATOR:
-        return 'Decorator'
+        return "Decorator"
     elif value == py_trees_msgs.Behaviour.BEHAVIOUR:
-        return 'Behaviour'
+        return "Behaviour"
     else:
         raise TypeError("invalid type specified in message [{}]".format(value))
 
@@ -84,12 +85,12 @@ def msg_constant_to_status_str(value: int) -> str:
         TypeError: if the status type is unrecognised
     """
     if value == py_trees_msgs.Behaviour.INVALID:
-        return 'INVALID'
+        return "INVALID"
     elif value == py_trees_msgs.Behaviour.RUNNING:
-        return 'RUNNING'
+        return "RUNNING"
     elif value == py_trees_msgs.Behaviour.SUCCESS:
-        return 'SUCCESS'
+        return "SUCCESS"
     elif value == py_trees_msgs.Behaviour.FAILURE:
-        return 'FAILURE'
+        return "FAILURE"
     else:
         raise TypeError("invalid status specified in message [{}]".format(value))
